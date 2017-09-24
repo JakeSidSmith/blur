8bdfe74f82610a81136b33b8724b4c9a4a43527a
Jake 'Sid' Smith <jake@dabapps.com>
2017-09-24 13:22:50 +0100

    Load images into canvas

--- index.js
+++ index.js
@@ -2,10 +2,39 @@
 
 (function () {
 
+  var fileReader;
   var filePicker = document.getElementById('file-picker');
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
+  var image = new Image();
 
+  if (!FileReader || !filePicker.files) {
+    alert('Sorry, you need a newer browser to use this app.');
+  }
 
+  function loadImageIntoCanvas () {
+    ctx.drawImage(image, 0, 0);
+  }
+
+  function loadImageAsSrc () {
+    image.src = fileReader.result;
+  }
+
+  function loadFile (event) {
+    var files = event.target.files;
+
+    if (files && files.length) {
+      fileReader.readAsDataURL(files[0]);
+    }
+  }
+
+  function init () {
+    fileReader = new FileReader();
+    fileReader.addEventListener('load', loadImageAsSrc);
+    filePicker.addEventListener('change', loadFile);
+    image.addEventListener('load', loadImageIntoCanvas);
+  }
+
+  init();
 
 })();
